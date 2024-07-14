0fc613f2c9d2dbb75704c6696f19091240cefde4
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2024-07-14 15:45:12 +0300

    darduino 1.8

--- arduino.sh
+++ arduino.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 # Starts the Arduino IDE using the docker image.
-# see also: ...
 
-#    -v /dev/ttyACM0:/dev/ttyACM0 \
-#    -v /dev/ttyUSB0:/dev/ttyUSB0 \
+set -e
+cd $(dirname $0)
+workdir=$(dirname $0)
+
 docker run \
-    -it \
-    --rm \
+    --rm --init \
+    --cap-drop ALL \
     --network=host \
     -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --mount type=bind,source='/tmp/.X11-unix/X0',target='//tmp/.X11-unix/X0',readonly \
     --device=/dev/ttyACM1 \
-    -v $HOME/research/arduino:/home/developer/Arduino \
-    -v $HOME/.arduino15:/home/developer/.arduino15 \
+    -v "${workdir}/developer:/home/developer" \
+    -v "$HOME/research/arduino:/home/developer/Arduino" \
     --name arduino \
-    hkorpi/darduino \
+    hkorpi/darduino-1.8 \
     arduino
 
