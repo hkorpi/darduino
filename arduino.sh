35a23ec6a5bdba5c4b4eb6d6c70cd63875524879
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2024-07-14 18:23:07 +0300

    fix device mounts

--- arduino.sh
+++ arduino.sh
@@ -12,11 +12,11 @@ docker run \
     --shm-size=2gb \
     -e DISPLAY=$DISPLAY \
     --tmpfs /run/dbus \
-    --mount type=bind,source='/tmp/.X11-unix/X0',target='//tmp/.X11-unix/X0',readonly \
+    --mount type=bind,source='/tmp/.X11-unix/X1',target='//tmp/.X11-unix/X1',readonly \
     -v "${workdir}/developer:/home/developer" \
     -v "$HOME/research/arduino:/home/developer/Arduino" \
     -v "$HOME/research/irrigation/arduino:/home/developer/Arduino/irrigation" \
-    --device=/dev/ttyACM1 \
+    --device=/dev/ttyACM0 \
     --name arduino \
     hkorpi/darduino \
     /opt/arduino-ide-2.2.1/start.sh
